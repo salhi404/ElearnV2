ab1d53f4125210f57f39c19a4aca297f358efbb3
salhi <salhinfo404@gmail.com>
2023-01-23 01:57:19 +0100

    what about now b

--- src/app/pages/interface/interface.component.ts
+++ src/app/pages/interface/interface.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
-  styleUrls: ["./interface.component.scss"]
+  styleUrls: ["./interface.component.scss","./interfacestyles.scss"]
 })
 export class InterfaceComponent implements OnInit {
 
