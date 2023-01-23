058f129c2cf45e7a872b49835f4353e2fd741164
salhi <salhinfo404@gmail.com>
2023-01-23 02:16:46 +0100

    interface deleate 1

--- src/app/pages/interface/interface.component.ts
+++ src/app/pages/interface/interface.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
-  styleUrls: ["./interface.component.scss","../../../assets/interfacestyles.scss"]
+  styleUrls: ["./interface.component.scss"]
 })
 export class InterfaceComponent implements OnInit {
 
