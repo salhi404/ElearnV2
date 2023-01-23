6f409451f552c93d3c74ec8b62a5b09b774ea3cd
salhi <salhinfo404@gmail.com>
2023-01-23 02:10:40 +0100

    interface style exeeded bugget  fix 1

--- src/app/pages/interface/interface.component.ts
+++ src/app/pages/interface/interface.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
-  styleUrls: ["./interface.component.scss","./interfacestyles.scss"]
+  styleUrls: ["./interface.component.scss","../../../assets/interfacestyles.scss"]
 })
 export class InterfaceComponent implements OnInit {
 
