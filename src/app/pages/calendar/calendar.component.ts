3114c5154486635731d8025f604b716126d2dadb
salhi <salhinfo404@gmail.com>
2023-01-27 03:03:02 +0100

    implemant add event with modal (no database sync)

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef, HostListener, ElementRef } from '@angular/core';
 import { Calendar, CalendarOptions, DateSelectArg, EventInput, EventSourceInput } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,6 +6,7 @@ import interactionPlugin from "@fullcalendar/interaction"
 
 import { DateClickArg } from '@fullcalendar/interaction';
 import { FullCalendarComponent } from '@fullcalendar/angular';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
@@ -14,15 +15,26 @@ import { FullCalendarComponent } from '@fullcalendar/angular';
 })
 export class CalendarComponent implements OnInit, AfterViewInit {
   @ViewChild(FullCalendarComponent) calendarElement!: FullCalendarComponent;
+  @ViewChild("modalDialog") modalDialog!: ElementRef;
+  blockHostListener:boolean=false;
   events: EventSourceInput = [
     /*{ title: 'event 1', date: '2023-01-01', },
     { title: 'event 2', start: new Date() , end:'2023-01-30',color:'red'}*/
   ]
+  form={
+    event:'',
+    startDate:'',
+    startTime:'',
+    endDate:'',
+    endTime:'',
+  }
+  datepipe: DatePipe = new DatePipe('en-US');
+  modelShowen:boolean=false;
   calendarTitle = "";
+  fadeModel=false;
   viewType: number = 0;
   datSelectionArg:DateSelectArg=null as any;
   calendarApi: Calendar = null as any;
-  event: EventInput = {};
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick, // MUST ensure `this` context is maintained
@@ -98,6 +110,54 @@ export class CalendarComponent implements OnInit, AfterViewInit {
   }
   addevent(){
     console.log(this.datSelectionArg);
+    const args=this.datSelectionArg;
+    if(!args){
+      console.log("select a date range");
+      
+    }else{
+      this.form.startDate=this.datepipe.transform(args.start,'yyyy-MM-dd')||'';
+      this.form.endDate=this.datepipe.transform(args.end,'yyyy-MM-dd')||'';
+      if(!args.allDay){
+        this.form.startTime=this.datepipe.transform(args.start,'hh:mm')||'';
+        this.form.endTime=this.datepipe.transform(args.end,'hh:mm')||'';
+      }
+
+      this.showModel();
+    }
+  }
+  showModel(){
+    this.blockHostListener=true;
+    this.modelShowen=true;
+    setTimeout(() => {
+      this.fadeModel=true;
+    }, 10);
+    setTimeout(() => {
+      this.blockHostListener=false;
+    }, 300);
+  }
+  closeModel(){
+    this.fadeModel=false;
+    this.form={event:'',startDate:'',startTime:'',endDate:'',endTime:'',}
+    setTimeout(() => {
+      this.modelShowen=false;
+    }, 150);
+  }
+  submitModal(){
+    if(this.form.event==='')this.form.event="My Event";
+    console.log(this.form);
+    const start:Date=new Date(this.form.startDate+'T'+(this.form.startTime!==''?this.form.startTime:'00:00'));
+    const end:Date=new Date(this.form.endDate+'T'+(this.form.endTime!==''?this.form.endTime:'00:00'));
+    const event:EventInput={ title: this.form.event,start:start,end:end,allDay:(this.form.startTime==''&&this.form.endTime=='') }
+    this.calendarApi.addEvent(event);
+    this.closeModel();
+    console.log("event");
+    console.log(event);
     
+  }
+  @HostListener('document:click', ['$event'])
+  clickout(event:any) {
+    if(!this.modalDialog.nativeElement.contains(event.target)&&!this.blockHostListener){
+      this.closeModel();
+    }
   }
 }
