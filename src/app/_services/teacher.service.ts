de7104e8d53aff2e8ab4488cd32d4c62b221daf4
salhi <salhinfo404@gmail.com>
2023-03-02 01:11:38 +0100

    teacher servise server url to railway

--- src/app/_services/teacher.service.ts
+++ src/app/_services/teacher.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StorageService } from './storage.service';
-const URL_API = 'http://192.168.1.103:3000/'; 
+// const URL_API = 'http://192.168.1.103:3000/'; 
 // const URL_API = 'https://frantic-colt-leather-jacket.cyclic.app/';
-// const URL_API = 'https://starter-express-api-production-816a.up.railway.app/';
+const URL_API = 'https://starter-express-api-production-816a.up.railway.app/';
 const TEACHER_API = URL_API+'api/teacher/';
 //Teacher service  
 const httpOptions = {
